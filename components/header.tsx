0e4f6596b3f940099423d46ecceb07d54076ac1c
ThiagoPuyelli <thiagopuyelli@gmail.com>
2021-10-14 19:27:01 -0300

    Maqueta header

---
+++ components/header.tsx
@@ -0,0 +1,61 @@
+import type { NextPage } from 'next'
+import styled from '@emotion/styled'
+
+const Header: NextPage = () => {
+  const HeaderStyled = styled.header`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    padding-left: 60px;
+    padding-right: 60px;
+    width: 100%;
+    height: 100px;
+    align-items: center;
+    border-bottom: 2px solid cyan;
+    .logo {
+      transition: 300ms all;
+    }
+    .logo:hover {
+      text-shadow: 0px 0px 3px black;
+    }
+    .linksHeader {
+      display: flex;
+      flex-flow: row wrap;
+      list-style: none;
+      .linkHeader {
+        margin: 20px;
+        font-size: 17px;
+        a {
+          transition: 300ms all;
+          display: block;
+        }
+        a:hover {
+          transform: scale(1.2, 1.2);
+        }
+      }
+    }
+  `
+
+  return (
+    <HeaderStyled>
+      <div className='logo'>
+        <a href='#'>
+          <h1>Crefo</h1>
+        </a>
+      </div>
+      <ul className='linksHeader'>
+        <li className='linkHeader'>
+          <a href='#'>Skills</a>
+        </li>
+        <li className='linkHeader'>
+          <a href='#'>Proyectos</a>
+        </li>
+        <li className='linkHeader'>
+          <a href='#'>Contacto</a>
+        </li>
+      </ul>
+    </HeaderStyled>
+  )
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
+import Header from '../components/header'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Header />
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
 }
 export default MyApp
